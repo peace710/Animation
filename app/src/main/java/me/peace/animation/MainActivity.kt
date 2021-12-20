1727db89776cd852f9e51932b0187e0fc51297bd
Peace710 <wxxinyun@163.com>
2021-12-20 11:31:44 +0800

    add motion layout basic 03 custom attribute

--- app/src/main/java/me/peace/animation/MainActivity.kt
+++ app/src/main/java/me/peace/animation/MainActivity.kt
@@ -21,7 +21,7 @@ import me.peace.animation.interpolator.InterpolatorActivity
 import me.peace.motion.MotionActivity
 
 class MainActivity : AppCompatActivity() {
-    private val list = arrayListOf<String>("SceneTransition","BasicTransition","CustomTransition","DrawableAnimation","Grid2Pager","Interpolator","Effect","MotionBasic01","MotionBasic02","MotionBasic02AutoCompleteFalse")
+    private val list = arrayListOf<String>("SceneTransition","BasicTransition","CustomTransition","DrawableAnimation","Grid2Pager","Interpolator","Effect","MotionBasic01","MotionBasic02","MotionBasic02AutoCompleteFalse","Motion03CustomAttribute")
 
     private val clazz = arrayListOf<Class<*>>(
         ActivitySceneTransition::class.java,
@@ -33,10 +33,11 @@ class MainActivity : AppCompatActivity() {
         EffectActivity::class.java,
         MotionActivity::class.java,
         MotionActivity::class.java,
+        MotionActivity::class.java,
         MotionActivity::class.java
         )
 
-    private val layout =  arrayListOf<Int>(R.layout.motion_01_basic,R.layout.motion_02_basic,R.layout.motion_02_basic_autocomplete_false)
+    private val layout =  arrayListOf<Int>(R.layout.motion_01_basic,R.layout.motion_02_basic,R.layout.motion_02_basic_autocomplete_false,R.layout.motion_03_custom_attribute)
 
     companion object{
         private const val MOTION_START = 7
